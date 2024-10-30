2cb3ea8a36c63d83bcbe2efc90d8b165d59a0bfc
jaredsahlberg <132834519+jaredsahlberg@users.noreply.github.com>
2024-10-30 16:31:46 +0000

    update project 05

---
+++ projects/05/article.php
@@ -0,0 +1,81 @@
+<?php include 'config.php'; ?>
+<?php include 'templates/head.php'; ?>
+<?php include 'templates/nav.php'; ?>
+
+<?php 
+   if (isset($_GET['id'])) {
+    // Prepare the SQL query to fetch the article based on the provided ID
+    $stmt = $pdo->prepare('SELECT articles.*, users.full_name AS author FROM articles JOIN users ON articles.author_id = users.id WHERE is_published = 1 AND articles.id = ?');
+        // Execute the query with the article ID
+    $stmt->execute([$_GET['id']]);
+        // Fetch the article as an associative array
+    $article = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Step 3: If an article with that ID does not exist, display an error message
+    if (!$article) {
+        
+        exit; // Stop further execution if no article is found
+    }
+} else {
+    
+    exit; // Stop further execution if no ID is provided
+}
+?>
+<!-- BEGIN YOUR CONTENT -->
+<section class="section">
+    <h1 class="title"><?= $article['title'] ?></h1>
+    <div class="box">
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-128x128">
+                    <img src="https://picsum.photos/128?random=<?= $article['id'] ?>">
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <?= $article['content'] ?>
+                    </p>
+                    <p>
+                        <small><strong>Author: <?= $article['author'] ?></strong>
+                            | Published: <?= time_ago($article['created_at']) ?>
+                            <?php if ($article['modified_on'] !== NULL) : ?>
+                                | Updated: <?= time_ago($article['modified_on']) ?>
+                            <?php endif; ?>
+                        </small>
+                    </p>
+                </div>
+                <p class="buttons">
+                    <a href="contact.php" class="button is-small is-info is-rounded">
+                        <span class="icon">
+                            <i class="fas fa-lg fa-hiking"></i>
+                        </span>
+                        <span><strong>Begin your journey now</strong></span>
+                    </a>
+                </p>
+                <p class="buttons">
+                    <a class="button is-small is-rounded">
+                        <span class="icon is-small">
+                            <i class="fas fa-thumbs-up"></i>
+                        </span>
+                        <span><?= $article['likes_count'] ?></span>
+                    </a>
+                    <a class="button is-small is-rounded">
+                        <span class="icon is-small">
+                            <i class="fas fa-star"></i>
+                        </span>
+                        <span><?= $article['favs_count'] ?></span>
+                    </a>
+                    <a class="button is-small is-rounded">
+                        <span class="icon is-small">
+                            <i class="fas fa-comment"></i>
+                        </span>
+                        <span><?= $article['comments_count'] ?></span>
+                    </a>
+                </p>
+            </div>
+        </article>
+    </div>
+</section>
+<?php include 'templates/footer.php'; ?>
+
+<!-- END YOUR CONTENT -->
