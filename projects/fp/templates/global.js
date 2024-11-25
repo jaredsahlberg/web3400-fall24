443b53bf8b7b76fb194e2ef92a2d4cb53f42484c
jaredsahlberg <132834519+jaredsahlberg@users.noreply.github.com>
2024-11-25 21:47:00 +0000

    update project fp

--- projects/fp/templates/global.js
+++ projects/fp/templates/global.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add event listener to all elements with the 'delete' class
-    document.querySelectorAll('.delete').forEach(function (button) {
+      document.querySelectorAll('.delete').forEach(function (button) {
         button.addEventListener('click', function () {
-            const notification = this.parentElement; // Parent of the close button
+            const notification = this.parentElement; 
             if (notification) {
-                notification.remove(); // Remove the notification element
+                notification.remove(); 
             }
         });
     });
